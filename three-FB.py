ea0adf7ba26fcf3889278876c14ce0a60c8d0e28
QiFuChina <517469138@qq.com>
2017-09-18 22:46:06 +0000

    solution 3

---
+++ three-FB.py
@@ -0,0 +1,11 @@
+# FizzBuzz
+# reference: http://wiki.c2.com/?FizzBuzzTest
+for x in range(1,100):
+		s = ""
+		if x % 3 == 0:
+			s += "Fizz"
+		if x % 5 == 0:
+			s += "Buzz"
+		if s == "":
+			s = x
+		print (s)
