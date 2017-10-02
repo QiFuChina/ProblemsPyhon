42748514c7ebaf487e063e6fef26a5389754801e
QiFuChina <517469138@qq.com>
2017-10-02 22:04:05 +0100

    solution 6

---
+++ six-Returnlist.py
@@ -0,0 +1,6 @@
+list=input("input elements:")
+# create a  list to store a set of elements from keyboard
+# print(list)
+# test the list store elements successful
+print("The biggest element is:"+max(list),"The smallest element is:"+min(list))
+# return the biggest and smallest elements in list 
