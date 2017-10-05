e7465f236a5467a8c66efaa46321fafe95c03259
QiFuChina <517469138@qq.com>
2017-10-05 23:05:54 +0100

    solution 8

--- eight-Merge.py
+++ eight-Merge.py
@@ -1,6 +1,15 @@
-list1=items.split("input list1:")
-list2=items.split("input list2:")
-list3=list1+list2
-list3.sort()
-print(list3)
+def merge(str):
+    # a function to list and sort
+    str=str1+str2
+    str=list(str)
+    # string can not be sorted by using sort function,but it can be changed to list type
+    str.sort()
+    print(str)
+
+if __name__=="__main__":
+    str1=input("input list1:")
+    str2=input("input list2:")
+    
+    merge(str)
+
 #print("list3:%s"%(list3.sort()))
